64f802e76450a0c1640933a349a7e92f0671d405
Milos Stojanovic <infomilosstojanovic@gmail.com>
2022-08-11 17:18:09 +0200

    Add #2

--- rdsCheckStop.sh
+++ rdsCheckStop.sh
@@ -6,19 +6,19 @@
 #Date           :27-January-2021
 #Author         :Milos Stojanovic
 #Contact:       :https://www.linkedin.com/in/infomilosstojanovic/
-###################################################################                                                                                                                                                                                                                                
+###################################################################
 str2='    "unreachable": true'
 array1=("ireland/test_mt" "ireland/env1" "ireland/env2" "virginia/env3" "brazil/env4")
 array2=("env1-db-all" "env2-db-all" "env3-db-all" "env4-db-all")
 array3=("eu-west-1" "eu-west-1" "us-east-1" "sa-east-1")
 array4=("prod" "dev" "dev" "stage")
-lenght=${#array1[@]}       
+lenght=${#array1[@]}
 for i in "${!array1[@]}"; do
-  str1=`ansible -i environments/aws/${array1[$i]}/inventory.yml host_group -m ping | grep '"unreachable": true'` 
+  str1=`ansible -i environments/aws/${array1[$i]}/inventory.yml host_group -m ping | grep '"unreachable": true'`
 if [[ $str1 == $str2 ]]; then
  aws rds stop-db-instance --db-instance-identifier ${array2[$i]} --region ${array3[$i]} --profile ${array4[$i]}
  echo "DB Instance ${array2[$i]} is stopped!"
-else 
+else
  echo "The environment for DB instance ${array2[$i]} is running! No need to stop!"
-fi                         
-done   
+fi
+done
